c495e82e587c33f04cbd3f00bc2fc6514f36aac5
AbdulLateef1158 <78403686+AbdulLateef1158@users.noreply.github.com>
2022-08-17 15:49:10 +0530

    Add files via upload

---
+++ HashsetDemo.java
@@ -0,0 +1,13 @@
+// Executed on  https://www.programiz.com/java-programming/online-compiler/
+import java.util.*;
+class HashsetDemo {
+	public static void main(String[] args) {
+		HashSet<Integer> ar=new HashSet<>();
+		ar.add(5);
+		ar.add(2);
+		ar.add(3);
+		ar.add(5);
+		ar.add(2);
+		System.out.println(ar);
+	}
+}
