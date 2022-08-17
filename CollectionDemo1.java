2078f0b62fd68d1f4df7b9cf513852b048906e70
AbdulLateef1158 <78403686+AbdulLateef1158@users.noreply.github.com>
2022-08-17 15:34:11 +0530

    Update CollectionDemo1.java

--- CollectionDemo1.java
+++ CollectionDemo1.java
@@ -3,10 +3,11 @@ import java.util.*;
 class CollectionDemo1{
 	public static void main(String[] args) {
 		Set<Integer> hs=new HashSet<Integer>();
-		for(int i=10;i>0;i--)
-		{
-			hs.add(i);
-		}
+		hs.add(5);
+		hs.add(2);
+		hs.add(3);
+		hs.add(1);
+		hs.add(4);
 		System.out.println(hs);
 	}
 }
