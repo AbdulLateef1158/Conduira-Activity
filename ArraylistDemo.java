bd45a6de7feb53c3162e30dfe0049f76d98bebb3
AbdulLateef1158 <78403686+AbdulLateef1158@users.noreply.github.com>
2022-08-17 15:40:22 +0530

    Add files via upload

---
+++ ArraylistDemo.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class ArraylistDemo {
+	public static void main(String[] args) {
+		ArrayList<Integer> ar=new ArrayList<>();
+		ar.add(5);
+		ar.add(2);
+		ar.add(3);
+		ar.add(1);
+		ar.add(4);
+		System.out.println(ar);
+	}
+}
